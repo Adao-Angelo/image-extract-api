e396c57a01ed52d62b4b0263fc317e3f3462d822
Adao-Angelo <adaobegginer@gmail.com>
2024-12-21 11:16:15 +0100

    updade main port

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def process_image_endpoint():
         return jsonify({"error": "Error downloading image"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=True)
