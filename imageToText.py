8f6958002ed42335dcb8956938d2cfb6154574b8
Adao-Angelo <adaobegginer@gmail.com>
2024-12-21 12:07:08 +0100

    create docs

--- imageToText.py
+++ imageToText.py
@@ -1,17 +1,15 @@
 
 import requests
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Accessing environment variables
-hf_key = os.getenv('HF_KEY')
+hf_key = os.getenv('HF_kEY')
 
 API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base"
 headers = {"Authorization": f"Bearer {hf_key}"}
 
 def query(filename):
+    print(hf_key)
     with open(filename, "rb") as f:
         data = f.read()
     response = requests.post(API_URL, headers=headers, data=data)
--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 from flask import Flask, request, jsonify
 from download import delete_image, download_image, process_image
 from imageToText import query
+from dotenv import load_dotenv
+from flasgger import Swagger
+
 
 app = Flask(__name__)
+Swagger(app)  
+load_dotenv()
 
 @app.route('/process-image', methods=['GET'])
 def process_image_endpoint():
+    """
+    Process an image from a URL and return its description.
+    ---
+    parameters:
+      - name: url
+        in: query
+        description: The URL of the image to be processed
+        required: true
+        type: string
+        format: uri
+    responses:
+      200:
+        description: The description of the processed image
+        content:
+          application/json:
+            schema:
+              type: object
+              example:
+                description: "Description of the processed image"
+      400:
+        description: URL not provided
+        content:
+          application/json:
+            schema:
+              type: object
+              example:
+                error: "URL not provided"
+      500:
+        description: Error in downloading or processing the image
+        content:
+          application/json:
+            schema:
+              type: object
+              example:
+                error: "Error downloading image"
+    """
     url = request.args.get('url')
     if not url:
         return jsonify({"error": "URL not provided"}), 400
@@ -20,4 +61,4 @@ def process_image_endpoint():
         return jsonify({"error": "Error downloading image"}), 500
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
